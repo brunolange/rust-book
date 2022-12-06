f5796b46d8ea93dc2046a2753a87d5242957d359
Bruno Lange <blangeram@gmail.com>
2022-12-05 20:25:51 -0800

    Adds integer overflow

--- integer-overflow/src/main.rs
+++ integer-overflow/src/main.rs
@@ -19,4 +19,33 @@ fn main() {
     let z: u8 = x + y;
 
     println!("The value of z = {z}");
+
+
+    let q = if true { () };
+
+    println!("The value of q = {:?}", q);
+
+    let w = 42;
+    f(w);
+    println!("The value of w = {w}");
+
+    let mut xs = [1, 2, 3, 4, 5];
+    g(&mut xs);
+    println!("The value of xs = {:?}", xs);
+
+    let x = 5;
+    let y = x;
+
+    println!("The value of x = {x}");
+    println!("The value of y = {y}");
+}
+
+fn f(mut x: i32) -> i32 {
+    x = x + 1;
+    x
+}
+
+fn g(xs: &mut [i32; 5]){
+    xs[0] = 100;
+    println!("xs = {:?}", xs);
 }
