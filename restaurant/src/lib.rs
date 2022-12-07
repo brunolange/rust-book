465c5b350385ae751de22d5a38b0061c147ae6f1
Bruno Lange <blangeram@gmail.com>
2022-12-06 21:41:37 -0800

    Moves each module to a separate file

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,15 +1,4 @@
-pub mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-        pub fn seat_at_table() {}
-    }
-
-    pub mod serving {
-        pub fn take_order() {}
-        pub fn serve_order() {}
-        pub fn take_payment() {}
-    }
-}
+pub mod front_of_house;
 
 pub use front_of_house::hosting;
 pub use front_of_house::serving;
@@ -20,20 +9,4 @@ pub fn visit() {
     serving::take_order();
     serving::serve_order();
     serving::take_payment();
-}
-
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
--- restaurant/src/main.rs
+++ restaurant/src/main.rs
@@ -1,6 +1,6 @@
 use restaurant::front_of_house::hosting;
 use restaurant::serving; // making use of pub-use
-use restaurant; // this is redundant
+// use restaurant; // this is redundant
 
 fn main() {
     hosting::add_to_waitlist();
